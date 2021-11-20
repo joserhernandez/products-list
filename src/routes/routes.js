75a420f40ed8b6a734762ae6b314900ac0f5ef71
joserhernandez <00366718@uca.edu.sv>
2021-11-20 17:58:48 -0600

    Add routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        status: 'Api Works'
-    });
+const Product = require('../models/product');
+
+router.get('/', async (req, res) => {
+
+    const products = await Product.find();
+    res.json('received');
 });
+
+router.post('/', async (req, res) => {
+    const { name, price } = req.body;
+    const product = new Product({
+        name,
+        price
+    });
+    await product.save();
+    res.json({status: `Product Saved`});
+});
+
+router.put('/:id', async (req, res) => {
+    const { name, price } = req.body;
+    const newProduct = { name, price };
+    await Product.findByIdAndUpdate(req.params.id, newProduct);
+    res.json({status: 'Product Updated'});
+})
+
+router.delete('/:id', async (req, res) => {
+    await Product.findByIdAndRemove(req.params.id);
+    res.json({status: 'Task Deleted'});
+})
 
 module.exports = router;
